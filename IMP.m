278ffe1da99783ee031832f0d10b499246e1966e
MOHAMED ERSHADH <ershadhrafi@gmail.com>
2020-11-01 11:16:47 +0530

    Importing Data

--- IMP.m
+++ IMP.m
@@ -1,3 +1,4 @@
+% An efficient way to Import Measurement Datasets 
 clc;
 clear all;
 close all;
@@ -6,8 +7,8 @@ str2='.DAT';
 for i=1:4
     for j=1:10
     str= strcat(str1,num2str(i-1),num2str(j-1),str2);
-AHALL.HA(i,j)=importdata(str,'s',13);
-if(i==4 && j==4)
+AHALL.HA(i,j)=importdata(str,'s',13); % Organizing into Data Strucutres
+if(i==4 && j==4) % Depends on the number of available grid points or (x,y) co-ordinate data for the currently imported dataset 
     break; end
     end  
 end
